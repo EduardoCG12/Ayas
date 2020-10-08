fd99e2ff066460b4ca819f89f8fcce91baf5f691
EduardoCG12 <eduardo.cosplayer@gmail.com>
2020-10-08 07:59:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ayashome/ThirdFragment.java
@@ -0,0 +1,42 @@
+package com.example.ayashome;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class ThirdFragment extends Fragment {
+
+    private Button btnPlay;
+    private Button btnPause;
+    private VideoView video;
+
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.reservar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_ThirdFragment_to_FirstFragment);
+            }
+        });
+    }
+}
