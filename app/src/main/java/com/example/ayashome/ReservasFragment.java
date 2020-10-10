621f051d857e462dfbea65c0bb1c10b123d1dfaa
Sergio <cortessergio059@gmail.com>
2020-10-10 13:46:40 +0200

    Arreglado Fecha, hora y guardar en bd las reservas

--- app/src/main/java/com/example/ayashome/ReservasFragment.java
+++ app/src/main/java/com/example/ayashome/ReservasFragment.java
@@ -56,55 +56,8 @@ public class ReservasFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 
-        final View root = inflater.inflate(R.layout.fragment_reservas, container, false);
-        reserva = root.findViewById(R.id.etReserva);
-        reservaFecha =  root.findViewById(R.id.etdFechaReserva);
-        etHora =  root.findViewById(R.id.etHora);
-        butGuardar = root.findViewById(R.id.butReservar);
-
-        /*reservaFecha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                showDatePickerDialog();
 
 
-            }
-        });*/
-
-        /*etHora.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                obtenerHora();
-
-
-            }
-        });*/
-
-        butGuardar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String fechaString = reservaFecha.getText().toString();
-                Hora = etHora.getText().toString();
-                fecha = ParseFecha(fechaString);
-
-                Log.e(Values.LOG_TAG,"Fecha Parseada : "+fecha);
-                Log.e(Values.LOG_TAG,"HORA : "+Hora);
-                Timestamp myDate = new Timestamp(fecha);
-
-
-                Map<String, Object> updateMap = new HashMap();
-                updateMap.put("Fecha", myDate);
-                updateMap.put("Hora", Hora);
-
-
-                db.collection("Reservas")
-                        .document(reserva.getText().toString())
-                        .set(updateMap);
-            }
-        });
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_reservas, container, false);
 
@@ -112,70 +65,16 @@ public class ReservasFragment extends Fragment {
 
     }
 
-    private void showDatePickerDialog() {
-        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                // +1 because January is zero
-                final String selectedDate = day + "/" + (month+1) + "/" + year;
-                reservaFecha.setText(selectedDate);
 
-            }
-        });
-
-        newFragment.show(this.getFragmentManager(),"datePicker");
-    }
-
-
-
-    private void obtenerHora(){
-        TimePickerDialog recogerHora = new TimePickerDialog(this.getContext(), new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                //Formateo el hora obtenido: antepone el 0 si son menores de 10
-                String horaFormateada =  (hourOfDay < 10)? String.valueOf(CERO + hourOfDay) : String.valueOf(hourOfDay);
-                //Formateo el minuto obtenido: antepone el 0 si son menores de 10
-                String minutoFormateado = (minute < 10)? String.valueOf(CERO + minute):String.valueOf(minute);
-                //Obtengo el valor a.m. o p.m., dependiendo de la selección del usuario
-                String AM_PM;
-                if(hourOfDay < 12) {
-                    AM_PM = "a.m.";
-                } else {
-                    AM_PM = "p.m.";
-                }
-                //Muestro la hora con el formato deseado
-                etHora.setText(horaFormateada + DOS_PUNTOS + minutoFormateado + " " + AM_PM);
-            }
-            //Estos valores deben ir en ese orden
-            //Al colocar en false se muestra en formato 12 horas y true en formato 24 horas
-            //Pero el sistema devuelve la hora en formato 24 horas
-        }, hora, minuto, false);
-
-        recogerHora.show();
-    }
-
-
-
-
-    public static Date ParseFecha(String fecha)
-    {
-
-        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-        Date fechaDate = null;
-        try {
-            fechaDate = formato.parse(fecha);
-
-        }
-        catch ( ParseException ex)
-        {
-            Log.e(Values.LOG_TAG, "Error creando fecha: " + ex.getMessage());
-            System.out.println(ex);
-        }
-        return fechaDate;
-    }
 
    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+       reserva = view.findViewById(R.id.etReserva);
+       reservaFecha =  view.findViewById(R.id.etdFechaReserva);
+       etHora =  view.findViewById(R.id.etHora);
+       butGuardar = view.findViewById(R.id.butReservar);
+
         view.findViewById(R.id.etdFechaReserva).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -212,9 +111,73 @@ public class ReservasFragment extends Fragment {
                         .set(updateMap);
 
 
+
+
                 /*NavHostFragment.findNavController(ReservasFragment.this)
                         .navigate(R.id.action_ReservasFragment_to_FirstFragment);*/
             }
         });
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because January is zero
+                final String selectedDate = day + "/" + (month+1) + "/" + year;
+                reservaFecha.setText(selectedDate);
+
+            }
+        });
+
+        newFragment.show(this.getFragmentManager(),"datePicker");
+    }
+
+
+
+    private void obtenerHora(){
+        TimePickerDialog recogerHora = new TimePickerDialog(this.getContext(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                //Formateo el hora obtenido: antepone el 0 si son menores de 10
+                String horaFormateada =  (hourOfDay < 10)? String.valueOf(CERO + hourOfDay) : String.valueOf(hourOfDay);
+                //Formateo el minuto obtenido: antepone el 0 si son menores de 10
+                String minutoFormateado = (minute < 10)? String.valueOf(CERO + minute):String.valueOf(minute);
+                //Obtengo el valor a.m. o p.m., dependiendo de la selección del usuario
+                String AM_PM;
+                if(hourOfDay < 12) {
+                    AM_PM = "A.M";
+                } else {
+                    AM_PM = "P.M";
+                }
+                //Muestro la hora con el formato deseado
+                etHora.setText(horaFormateada + DOS_PUNTOS + minutoFormateado + " " + AM_PM);
+            }
+            //Estos valores deben ir en ese orden
+            //Al colocar en false se muestra en formato 12 horas y true en formato 24 horas
+            //Pero el sistema devuelve la hora en formato 24 horas
+        }, hora, minuto, false);
+
+        recogerHora.show();
+    }
+
+
+
+
+    public static Date ParseFecha(String fecha)
+    {
+
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date fechaDate = null;
+        try {
+            fechaDate = formato.parse(fecha);
+
+        }
+        catch ( ParseException ex)
+        {
+            Log.e(Values.LOG_TAG, "Error creando fecha: " + ex.getMessage());
+            System.out.println(ex);
+        }
+        return fechaDate;
     }
 }
