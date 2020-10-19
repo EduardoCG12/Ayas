d2f8665b5e8802f95dd9e8e987b95cd9beb3207e
EduardoCG12 <eduardo.cosplayer@gmail.com>
2020-10-19 09:31:33 +0200

    haciendo detalles mejorados y nombres cambiados

--- app/src/main/java/com/example/ayashome/BotonesFragment.java
+++ app/src/main/java/com/example/ayashome/BotonesFragment.java
@@ -4,17 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class BotonesFragment extends Fragment {
+    public EditText editText;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
+
     ) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_botones, container, false);
@@ -29,11 +34,17 @@ public class BotonesFragment extends Fragment {
 
                 NavHostFragment.findNavController(BotonesFragment.this)
                         .navigate(R.id.action_BotonesFragment_to_TextoFragment);
-                //FragmentManager fragmentManager = getFragmentManager();
-                //FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                //fragmentTransaction.replace(R.id.ThirdFragment, mFeedFragment);
-                //fragmentTransaction.addToBackStack(null);
-                //fragmentTransaction.commit();
+
+                /*FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+
+                TextoFragment textoFragment = new TextoFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("texto1", editText.getText.toString());
+                textoFragment.setArguments(bundle);
+                ft.replace(R.id.TextoFragment, textoFragment);
+                ft.addToBackStack(null);
+                ft.commit();*/
 
 
             }
