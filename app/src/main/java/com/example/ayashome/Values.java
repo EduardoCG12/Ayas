d988325d4e02a6b37e84104371b9e5e51d1126f4
EduardoCG12 <eduardo.cosplayer@gmail.com>
2020-10-20 10:22:09 +0200

    opciones toolbar

--- app/src/main/java/com/example/ayashome/Values.java
+++ app/src/main/java/com/example/ayashome/Values.java
@@ -1,3 +1,15 @@
 package com.example.ayashome;
 
+public class Values {
 
+    public static final String LOG_TAG = "tag_ayas";
+
+
+    // LOGIN
+    public static final int RC_SIGN_IN = 0;
+    public static final int RC_MAIN_ACTIVITY = 1;
+
+    public static final int REQ_ACT_2 = 8;
+
+
+}
