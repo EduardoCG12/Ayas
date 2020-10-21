37ef1439f8534edb3481cc7ee128b6d3e271be50
EduardoCG12 <eduardo.cosplayer@gmail.com>
2020-10-21 11:45:02 +0200

    tocando pequeños detalles

---
+++ app/src/main/java/com/example/ayashome/Opciones.java
@@ -0,0 +1,14 @@
+package com.example.ayashome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Opciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opciones);
+    }
+}
