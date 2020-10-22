a4e94c3391e30d5e63fd6eb870eaf851d04824d8
EduardoCG12 <eduardo.cosplayer@gmail.com>
2020-10-22 11:23:10 +0200

    tocando pequeños detalles

--- app/src/main/java/com/example/ayashome/Values.java
+++ app/src/main/java/com/example/ayashome/Values.java
@@ -8,4 +8,8 @@ public class Values {
     // LOGIN
     public static final int RC_SIGN_IN = 0;
     public static final int RC_MAIN_ACTIVITY = 1;
+
+    public static final int REQ_ACT_2 = 8;
+
+
 }
